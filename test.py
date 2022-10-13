e9ea6ddd0830616344ba0666a2d0f7f6943899bd
Kevin <kevin.wei20622@gmail.com>
2022-10-13 17:14:43 -0400

    sync

---
+++ test.py
@@ -0,0 +1,14 @@
+import codecs
+import os
+from urllib.request import urlopen
+
+import bs4 as bs
+from bs4 import BeautifulSoup
+
+# html = urlopen('file:///' + os.path.abspath('test.html')); #reads html from
+html = codecs.open("test.html", "r", "utf-8")
+
+replacement_soup = BeautifulSoup(html, 'html.parser')  #parses html file
+
+if __name__ == '__main__':
+    print(">" + str(replacement_soup))
