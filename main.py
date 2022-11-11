3b0f5d6a9f8dfa26abbfdd35c1d227f2b86d5109
Kevin <kevin.wei20622@gmail.com>
2022-11-11 00:12:40 -0500

    Configure parsing info with regex

--- main.py
+++ main.py
@@ -10,35 +10,41 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-import os #allows for directory manipulation
-import webbrowser #allows for displaying websites in browsers
+import os  # allows for directory manipulation
+import webbrowser  # allows for displaying websites in browsers
 
-#selenium automates browsers
-#chromedriver targets chrome
+# NOTE
+# selenium automates browsers
+# chromedriver targets chrome
 
+driver = webdriver.Chrome(ChromeDriverManager().install())   # open chrome through driver
+driver.get("https://www.costco.ca/laptops.html")    # opens link
+# Bestbuy Link
+# https://www.bestbuy.ca/en-ca/category/laptops-macbooks/20352
+
+# Option 2
 # chrome_options = Options()
 # driver = webdriver.Chrome(executable_path=r"C:\Users\Kevin\Downloads\chromedriver_win32.zip\chromedriver.exe", options=chrome_options)
 
-driver = webdriver.Chrome(ChromeDriverManager().install())   #open chrome through driver
-driver.get("https://www.costco.ca/laptops.html")    #opens link
-# https://www.bestbuy.ca/en-ca/category/laptops-macbooks/20352
+# Option 3: Antiwebscraping Blocked
+# page = requests.get(URL)
 
-target_soup = BeautifulSoup(driver.page_source, 'lxml')  #parses site
 
+target_soup = BeautifulSoup(driver.page_source, 'lxml')  # parses site
+
+html = codecs.open("test.html", "r", "utf-8")  # reads local html
+replacement_soup = BeautifulSoup(html, 'lxml')  # html -> Soup parsing
+
+# Option 2
 # html = urlopen('file:///' + os.path.abspath('test.html')); #reads html from
-html = codecs.open("index.html", "r", "utf-8") #same thing
-replacement_soup = BeautifulSoup(html, 'lxml')  #parses html file
 
+# NOTE
 # html.parser- built-in - no extra dependencies needed
 # html5lib - the most lenient - better use it if HTML is broken
 # lxml - the fastest
 #
 # lenient parsing: can interpret inputs that do not match strict formats
 
-
-
-# URL = "https://www.bestbuy.ca/en-ca/category/laptops-macbooks/20352"
-# page = requests.get(URL)
 
 class Item:
     name = ""
@@ -51,8 +57,8 @@ class Item:
     out_of_stock = False
 
     def __init__(self, name, html):
-        Item.name = name
-        Item.html = html
+        self.name = name
+        self.html = html
 
 
 item_list = []
@@ -61,26 +67,25 @@ item_list = []
 if __name__ == '__main__':
     # print(soup.prettify())
 
-    target_soup.prettify()
+    target_soup.prettify()  # Soup -> String
 
-    #extracts all items on site
-    html_item_list = target_soup.find_all("span", {'class' : 'description'})
+    html_item_list = target_soup.find_all("span", {'class' : 'description'})  # extracts all items on site
 
-    # for i in range(len(html_item_list)):
-    #     cur_html = html_item_list[i]
-    #     cur_link = re.findall('^href="([^"]*)"$', str(html_item_list[i]))[0]
-    #     # item_list[i].name = re.findall(item_list[i].link + '">([^<]*)<', html_item_list[i])
-    #     item_list.append(Item(cur_html, cur_link))
-    #     print(item_list[i].link)
+    # stores data from html into Item object
+    for i in range(len(html_item_list)):
+        cur_html = html_item_list[i]
+        item_as_string = str(html_item_list[i])
+        item_as_string_front_chopped = item_as_string[item_as_string.find("href"):]
+        cur_link = re.findall(r'href="([^"]*)"', item_as_string_front_chopped)[0]
+        # item_list[i].name = re.findall(item_list[i].link + '">([^<]*)<', html_item_list[i])
+        item_list.append(Item(cur_html, cur_link))
+        print(item_list[i].link)
 
+    items_as_string = ""  # converts items into string awaiting insert to HTML
 
-    items_as_string = ""   #converts items into string awaiting insert to HTML
-
-    #turns list of items into a chunk of html text
+    # turns list of items into a chunk of html text
     for item in html_item_list:
         items_as_string += str(item) + "\n"
-
-    # print(items_as_string)
 
     #reads html file
     # with open(r"C:\Users\Kevin\Desktop\Dev\Webscraper\test.html", 'r') as f:
@@ -89,19 +94,23 @@ if __name__ == '__main__':
 
         # replacement_soup = BeautifulSoup(f.read());
 
+    title = "<h1>\n Welcome to Scrap Yard\n </h1> \n"
+
     # replacement_soup.body.append("<body>" + items_as_string + "</body>", 'html.parser')
-    replacement_soup.find("head").insert_after("<body>" + items_as_string + "</body>")
+    replacement_soup.find("body").replace_with("<body> \n" + title + items_as_string + "</body>")
 
+    with open(r"C:\Users\Kevin\Desktop\Dev\Webscraper\test.html", 'w') as f:
+        new_str = str(replacement_soup.prettify())  # Soup -> String
 
-  
-    with open(r"C:\Users\Kevin\Desktop\Dev\Webscraper\index.html", 'w') as f:
-        new_str = str(replacement_soup.prettify()) #converts soup into string
-        new_str = new_str.replace("&lt;", "<") #fixes "<" and ">" in html
+        # fixes "<" and ">" in html
+        new_str = new_str.replace("&lt;", "<")
         new_str = new_str.replace("&gt;", ">")
-        new_str = new_str.replace("</a>", "</a> <br>") #adds some line breaks
-        f.write(new_str) #inputs into html file
 
-        driver.quit() #closes chrome driver
+        new_str = new_str.replace("</a>", "</a> <br>")  # adds some line breaks
+
+        f.write(new_str)  # inputs into html file
+
+        driver.quit()  # closes chrome driver
 
 
 
