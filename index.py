5a4b3f22ddb6ba9733a092f8573e84b57fa12b94
Kevin <kevin.wei20622@gmail.com>
2022-11-23 16:28:00 -0500

    Configure page navigation Add about page

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import scrape
 # follows directory structure to get html and css files
 app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
@@ -9,9 +9,15 @@ def index():
     return render_template('test.html')
 
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/scrape/', methods=['POST'])
 def activate_scrape():
     scrape.main()
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
